6e1f1dff01e3c0b597802329a696228e2f7e5812
buhe <bugu1986@126.com>
2023-12-24 21:37:05 +0800

    "Add executable product \"gemini-pro-cli\" to Package.swift."

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "gemini-pro-cli",
+    products: [
+        .executable(name: "gemini-pro-cli", targets: ["gemini-pro-cli"]),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
