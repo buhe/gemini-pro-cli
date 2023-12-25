8969e0afe850f70722249813e5f110b16d180719
buhe <bugu1986@126.com>
2023-12-25 13:54:50 +0800

    "Add import statement for Foundation and handle environment variable GOOGLEAI_API_KEY in Execute struct's main function."

--- Sources/exe.swift
+++ Sources/exe.swift
@@ -1,9 +1,18 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
 import LangChain
+import Foundation
+
 @main
 struct Execute {
     static func main() async throws {
+        if let myEnvVar = ProcessInfo.processInfo.environment["GOOGLEAI_API_KEY"] {
+            print("The value of GOOGLEAI_API_KEY is \(myEnvVar)")
+            Env.initSet(["GOOGLEAI_API_KEY": myEnvVar])
+        } else {
+            print("MY_ENV_VAR is not set")
+        }
+        
         let llm = Gemini()
         let r = await llm.generate(text: "hi")
         print("Hello, world!! \(r!.llm_output!)")
