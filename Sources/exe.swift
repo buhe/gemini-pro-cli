1a0b22342d5cf3ea76436b0d54482168d15b7e8a
buhe <bugu1986@126.com>
2023-12-25 15:00:32 +0800

    "Refactor exe.swift to use a constant for the Google AI API key and improve code readability."

--- Sources/exe.swift
+++ Sources/exe.swift
@@ -1,29 +1,25 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
 import LangChain
 import Foundation
 
 @main
 struct Execute {
+    static let GKEY = "GOOGLEAI_API_KEY"
     static func main() async throws {
-        if let myEnvVar = ProcessInfo.processInfo.environment["GOOGLEAI_API_KEY"] {
-//            print("The value of GOOGLEAI_API_KEY is \(myEnvVar)")
-            Env.initSet(["GOOGLEAI_API_KEY": myEnvVar])
+        if let myEnvVar = ProcessInfo.processInfo.environment[GKEY] {
+            Env.initSet([GKEY: myEnvVar])
         } else {
-            print("GOOGLEAI_API_KEY is not set")
+            print("\(GKEY) is not set")
         }
         let arguments = CommandLine.arguments
         var prompt = ""
         for (index, argument) in arguments.enumerated() {
             if index > 1 {
-//                print("Argument \(index): \(argument)")
                 prompt = "\(prompt) \(argument)"
             }
             if index == 1 {
                 prompt = argument
             }
         }
-//        print(prompt)
         let llm = Gemini()
         let r = await llm.generate(text: prompt)
         print("🎄\(r!.llm_output!)")
